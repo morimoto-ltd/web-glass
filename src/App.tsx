5e282f7195e4b78f74bc6a085f6d9c0b6a28f499
Dmitry Kokhanevych <dmitry.kokhanevich@gmail.com>
2025-06-14 19:26:54 +0300

    Preview V1

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+/*
 function App() {
   const [count, setCount] = useState(0)
 
@@ -30,6 +31,35 @@ function App() {
       </p>
     </>
   )
+}
+*/
+
+function Icon({ isStarred }: { isStarred: boolean }) {
+    return isStarred
+        ? <img src="star-starred.png" />
+        : <img src="star.png" />;
+}
+
+function App() {
+    const [isStarred, setIsStarred] = useState(false);
+    return (
+        <section role="main" className="App">
+            <div className="App__wrapper">
+                <img
+                    className="App__background"
+                    src="https://plus.unsplash.com/premium_photo-1665657351698-7d61f4b57ddc?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                />
+                <button
+                    className="App__liquidGlass__wrapper"
+                    onClick={() => setIsStarred(!isStarred)}
+                >
+                    <Icon isStarred={isStarred}/>
+                    <div className="App__liquidGlass" />
+                    <div className="App__liquidGlass-backdrop" />
+                </button>
+            </div>
+        </section>
+    );
 }
 
 export default App
